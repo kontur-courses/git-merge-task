f84b0305d16b0424950f0f49034601c7e8c64a44
Pavel Egorov <xoposhiy@mail.ru>
2018-11-14 00:32:46 +0500

    refactor tests

--- Calculator_Should.cs
+++ Calculator_Should.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 
 namespace Kontur.Courses.Git
@@ -5,29 +6,40 @@ namespace Kontur.Courses.Git
 	[TestFixture]
 	public class Calculator_Should
 	{
+		private Calculator calc;
+
+		[SetUp]
+		public void SetUp()
+		{
+			calc = new Calculator();
+		}
+
+		public Maybe<double> Calc(string input)
+		{
+			var args = input.Split(new []{' '}, StringSplitOptions.RemoveEmptyEntries);
+			return calc.Calculate(args);
+		}
+
 		[Test]
 		public void OneArg()
 		{
-			var calc = new Calculator();
-			Assert.AreEqual(42, calc.Calculate(new[] { "42" }).Value);
-			Assert.AreEqual(43, calc.Calculate(new[] { "43" }).Value);
+			Assert.AreEqual(42, Calc("42").Value);
+			Assert.AreEqual(43, Calc("43").Value);
 		}
 
 		[Test]
 		public void ZeroArg()
 		{
-			var calc = new Calculator();
-			Assert.AreEqual(42, calc.Calculate(new[] { "42" }).Value);
-			Assert.AreEqual(42, calc.Calculate(new string[] { }).Value);
-			Assert.AreEqual(42, calc.Calculate(new string[] { }).Value);
+			Assert.AreEqual(42, Calc("42").Value);
+			Assert.AreEqual(42, Calc("").Value);
+			Assert.AreEqual(42, Calc("").Value);
 		}
 
 		[Test]
 		public void ThreeArg()
 		{
-			var calc = new Calculator();
-			Assert.AreEqual(55, calc.Calculate(new[] { "42", "+", "13" }).Value);
-			Assert.AreEqual(1, calc.Calculate(new[] { "2", "-", "1" }).Value);
+			Assert.AreEqual(55, Calc("42 + 13").Value);
+			Assert.AreEqual(1, Calc("2 - 1").Value);
 		}
 	}
 }
