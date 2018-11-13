a302785779b141b6612548b61b46cb12fdcd051c
Pavel Egorov <xoposhiy@mail.ru>
2018-11-14 01:03:33 +0500

    handle incorrect user input

--- Calculator.cs
+++ Calculator.cs
@@ -3,21 +3,38 @@ namespace Kontur.Courses.Git
 	public class Calculator
 	{
 		private Maybe<double> lastResult = 0;
+		private Maybe<double> TryParseDouble(string s)
+		{
+			double v;
+			if (double.TryParse(s, out v))
+				return v;
+			return Maybe<double>.FromError("Not a number '{0}'", s);
+		}
 
 		public Maybe<double> Calculate(string[] args)
 		{
 			if (args.Length == 0)
 				return lastResult;
 			if (args.Length == 1)
-				return lastResult = double.Parse(args[0]);
+			{
+				var result = TryParseDouble(args[0]);
+				if (result.HasValue)
+					lastResult = result;
+				return result;
+			}
 			if (args.Length == 3)
 			{
-				var v1 = double.Parse(args[0]);
-				var v2 = double.Parse(args[2]);
-				return lastResult = Execute(args[1], v1, v2);
+				var v1 = TryParseDouble(args[0]);
+				var v2 = TryParseDouble(args[2]);
+				if (!v1.HasValue || !v2.HasValue) return v1;
+				var result = Execute(args[1], v1.Value, v2.Value);
+				if (result.HasValue)
+					lastResult = result;
+				return result;
 			}
 			return Maybe<double>.FromError("Error input");
 		}
+
 
 		private Maybe<double> Execute(string op, double v1, double v2)
 		{
--- Calculator_Should.cs
+++ Calculator_Should.cs
@@ -29,5 +29,25 @@ namespace Kontur.Courses.Git
 			Assert.AreEqual(55, calc.Calculate(new[] { "42", "+", "13" }).Value);
 			Assert.AreEqual(1, calc.Calculate(new[] { "2", "-", "1" }).Value);
 		}
+
+		[TestCase("asd", "+", "2")]
+		[TestCase("2", "asd", "3")]
+		public void ThreeArg_BadInput(params string[] args)
+		{
+			var calc = new Calculator();
+			calc.Calculate(new[] { "5" });
+			Assert.IsFalse(calc.Calculate(args).HasValue);
+			Assert.AreEqual(5.0, calc.Calculate(new string[] { }).Value);
+		}
+
+		[Test]
+		public void OneArg_BadInput()
+		{
+			var calc = new Calculator();
+			calc.Calculate(new[] { "5" });
+			Assert.IsFalse(calc.Calculate(new[] { "asd" }).HasValue);
+			Assert.AreEqual(5.0, calc.Calculate(new string[] { }).Value);
+		}
+
 	}
 }
